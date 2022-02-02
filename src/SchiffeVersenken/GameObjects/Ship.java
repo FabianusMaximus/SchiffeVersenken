574314bb6d1dabaee0b8bef26ff0cf54c714063f
FabianusMaximus <leppfabian@gmail.com>
2022-02-02 16:43:31 +0100

    Update Ship.java

--- src/SchiffeVersenken/GameObjects/Ship.java
+++ src/SchiffeVersenken/GameObjects/Ship.java
@@ -1,14 +1,40 @@
 package SchiffeVersenken.GameObjects;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Ship {
     private String name;
     private int value;
     private int groesse;
+    private Orientation orientation;
 
-    public Ship(String pName, int pValue, int pGroesse){
+    public Ship(String pName, int pValue, int pGroesse) {
         name = pName;
         value = pValue;
         groesse = pGroesse;
+        orientation = Orientation.HORIZONTAL;
+
+    }
+
+    public ArrayList<Point> applyOrientation(int x, int y) {
+        ArrayList<Point> nextIndexes = new ArrayList<Point>();
+        if (orientation == Orientation.HORIZONTAL) {
+            for (int i = 0; i < groesse; i++) {
+                nextIndexes.add(new Point(x, y + i));
+            }
+        } else {
+            for (int i = 0; i < groesse; i++) {
+                nextIndexes.add(new Point(x + i, y));
+
+            }
+        }
+        return nextIndexes;
+
+    }
+
+    public void changeOrientation() {
+        orientation = orientation.flip();
     }
 
     public void setName(String name) {
@@ -33,5 +59,16 @@ public class Ship {
 
     public int getGroesse() {
         return groesse;
+    }
+}
+
+enum Orientation {
+    HORIZONTAL, VERTICAL;
+
+    Orientation flip() {
+        if (this.equals(HORIZONTAL)) {
+            return VERTICAL;
+        }
+        return HORIZONTAL;
     }
 }
